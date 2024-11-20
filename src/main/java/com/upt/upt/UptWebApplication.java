cec0c20efc57712d575d20382071cdd5e0e923e3
Gabzers <gabrielproenca40@gmail.com>
2024-11-20 01:28:55 +0000

    first commit

---
+++ src/main/java/com/upt/upt/UptWebApplication.java
@@ -0,0 +1,13 @@
+package com.upt.upt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UptWebApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/upt/upt/UptWebController.java
@@ -0,0 +1,13 @@
+package com.upt.upt;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UptWebController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; 
+    }
+}
