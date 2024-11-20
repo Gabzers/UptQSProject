9e77c390ddfe2d1275033ebd266b21bc3541fd64
Gabzers <gabrielproenca40@gmail.com>
2024-11-20 22:11:16 +0000

    Website ✔

--- src/main/java/com/upt/upt/UptWebController.java
+++ src/main/java/com/upt/upt/UptWebController.java
@@ -8,6 +8,31 @@ public class UptWebController {
 
     @GetMapping("/")
     public String home() {
-        return "user"; 
+        return "user";  // Página inicial que você deseja exibir
+    }
+
+    @GetMapping("/back")
+    public String back() {
+        return "user";  // Redireciona para a página user.html quando o botão "Back" é clicado
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        return "login";  // Redireciona para a página login.html quando o botão "Back" é clicado
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "user";  // Outra página de usuário, caso necessário
+    }
+
+    @GetMapping("/edit")
+    public String edit() {
+        return "userEvaluation";  // Redireciona para a página user.html quando o botão "Back" é clicado
+    }
+
+    @GetMapping("/createUC")
+    public String createUC() {
+        return "userUC";  // Redireciona para a página createUC.html quando o botão "Back" é clicado
     }
 }
